ecb2bb7024bce512a764280c6459778898adae4b
Minami Munakata <june28munazo@gmail.com>
2019-04-08 19:32:42 -0700

    Create an input form with jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// function getButtonText() {
+//     return 'Click on me!'
+// }
+
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+    // const buttonText = 'Click Me!';
+    const buttonText = {text: 'Click Me'}
+    // const style = {backgroundColor: 'blue', color: 'white'};
+    const label = 'Enter name:'
+
+    return (
+        <div>
+        <label className="label" htmlFor="name">
+            {label}
+        </label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {/* <button style={style}> */}
+            {buttonText.text}
+            {/* {getButtonText()} */}
+        </button>
+        </div>        
+    );
 };
 
 // Take the react component and show it on the screen
